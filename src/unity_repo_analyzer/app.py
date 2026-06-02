f19cdb6969782ad54bca0fa87b109c4e6dc321d0
Mehwish Tariq <mta.34091@gmail.com>
2026-06-02 11:13:08 +0500

    Update app.py

--- src/unity_repo_analyzer/app.py
+++ src/unity_repo_analyzer/app.py
@@ -109,4 +109,4 @@ if __name__ == "__main__":
         )
 
     # Launch Gradio UI
-    ui_dashboard.launch(server_name="127.0.0.1", server_port=GRADIO_PORT)
+    ui_dashboard.launch(server_name="0.0.0.0", server_port=GRADIO_PORT)
